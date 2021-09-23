e782e6435c3b74f5bcc5d7d0e579140657619701
Leticia Fontoura <L.Wolff@ibm.com>
2021-09-22 23:52:16 -0300

    added some other styles

--- src/aside/aside.tsx
+++ src/aside/aside.tsx
@@ -12,14 +12,14 @@ interface FilesType {
 const Files: FilesType[] = [
   {
     id: '1',
-    name: 'Lala',
+    name: 'Lala.md',
     content: 'Oi',
     active: false,
     status: 'saved',
   },
   {
     id: '2',
-    name: 'Lala2',
+    name: 'Lala2.md',
     content: 'Oi2',
     active: false,
     status: 'saved',
@@ -29,12 +29,18 @@ const Files: FilesType[] = [
 function Aside () {
   return (
     <AsideMenu>
-      <img src={DarkLogo} alt='dark logo' />
+      <AsideHeader>
+        <img src={DarkLogo} alt='dark logo' />
+        <Heading>
+          Arquivos
+        </Heading>
+      </AsideHeader>
+      <AsideButton><span>+</span> Adicionar arquivo</AsideButton>
       {Files.map((file) => {
         return (
           <ListItems key={file.id}>
-            <ListA href='/'>{file.content}</ListA>
-            <button>x</button>
+            <ListA href='/'>{file.name}</ListA>
+            <ListItemButton>x</ListItemButton>
           </ListItems>
         )
       })}
@@ -45,7 +51,36 @@ function Aside () {
 const AsideMenu = styled.aside`
   ${({ theme }) => css`
     background-color: ${theme.colors.black};
-    width: 300px;
+    width: 332px;
+  `}
+`
+
+const AsideHeader = styled.header``
+
+const Heading = styled.h1`
+  ${({ theme }) => css`
+    font-size: 1.6rem;
+    color: ${theme.colors.white};
+    padding-left: 30px;
+    font-weight: 500;
+
+    &::before {
+      content: "";
+      width: 13px;
+      border-top: 2px solid ${theme.colors.primary};
+      position: absolute;
+      top: 77px;
+      left: 7px;
+    }
+
+    &::after {
+      content: "";
+      width: 178px;
+      border-top: 2px solid ${theme.colors.primary};
+      position: absolute;
+      top: 77px;
+      left: 110px;
+    }
   `}
 `
 
@@ -59,12 +94,42 @@ const ListItems = styled.li`
 const ListA = styled.a`
   ${({ theme }) => css`
     text-decoration: none;
-    font-size: 2rem;
+    font-size: 1.6rem;
     color: ${theme.colors.white};
 
     &:visited {
       text-decoration: none;
       color: ${theme.colors.white};
+    }
+  `
+  }
+`
+
+const ListItemButton = styled.button`
+  ${({ theme }) => css`
+    background-color: transparent;
+    color: ${theme.colors.white};
+    border: 0;
+    font-size: 2rem;
+    cursor: pointer;
+  `
+  }
+`
+
+const AsideButton = styled.button`
+  ${({ theme }) => css`
+    background-color: ${theme.colors.primary};
+    color: ${theme.colors.lightBlack};
+    font-size: 1.3rem;
+    border: 0;
+    border-radius: 3px;
+    margin: 0 32px;
+    padding: 10px 0;
+    width: 268px;
+    cursor: pointer;
+
+    & span {
+      font-size: 1.9rem;
     }
   `
   }
